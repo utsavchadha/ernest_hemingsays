a0c303e5199fba8ca4b4a44e04948600e5f96f39
Utsav Chadha <utsavchadha@Utsavs-MBP.home>
2017-10-10 04:55:18 -0400

    Adding config.

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+	consumer_key: 'l4T9lmcozGDVpIP89KjEV3ZT6',
+	consumer_secret: 'YFAUrGWmtIyldc1jczVNW1FzDWA2U2pQ3zGpjrx7JmoRdCjMCh',
+	access_token: '917617708706992128-qIKNMQJNIIvcbcm8dgdU6DwFT5Kgqiu',
+	access_token_secret: 'QLxKKf5sFN2MHJ5DEkgNkeBo1THyFHUAr1cYbjFPiKuSh'
+}
